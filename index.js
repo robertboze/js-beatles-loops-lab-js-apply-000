f47a075234be062c525b74de7ee6bd886c2f4516
ineffable-buffer-3376 <robertboze5@gmail.com>
2018-10-30 15:49:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // add solution here
+var theBeatlesPlay(arr1,arr2){
+
+var arr3 = [];
+
+  for(i=0;i<=arr1.length();I++){
+
+  var musician = arr[i];
+  
+    for(j=i;j<=arr2.length();j++){
+
+      var instrument = arr2[i];
+       
+    }
+var sentence = musician + "plays" + instrument;
+
+    arr3.push(sentence);
+  }
+  return arr3;
+  
+}
