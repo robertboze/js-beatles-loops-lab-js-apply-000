21e793255ed20e9efd9b1c3daae32ccae76d0e1b
ineffable-buffer-3376 <robertboze5@gmail.com>
2018-10-30 15:57:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,22 @@ function theBeatlesPlay(musicians, instruments) {
   return newArr;
 
 }
+
+var facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+function johnLennonFacts(arr) {
+  var result= []; 
+  var i = 0; 
+  while(i < arr.length) {
+    result.push(arr[i] + '!!!')
+    i++; 
+  }
+  return result; 
+}
+
+johnLennonFacts(facts); 
